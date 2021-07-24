2315ea1bc723ecb72e6569953f8ed733c50eb1ed
Firoso <daniel@danielgreen.net>
2021-07-24 16:56:29 -0700

    adding pieces support

---
+++ JotunnModStub/Match.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace RecipeRewriter
+{
+    public class Match
+    {
+        public Match(string name, int? amount, string buildTool)
+        {
+            this.Name = name;
+            this.Amount = amount;
+            this.BuildTool = buildTool;
+        }
+
+        public string Name { get; set; }
+        public int? Amount { get; set; }
+        public string BuildTool { get; set; }
+
+        public static Match FromJsonObjectWithMatchProperty(JObject piece)
+        {
+            // match a piece
+            var matchProperty = piece.Property("match", StringComparison.InvariantCultureIgnoreCase).Value.Value<JObject>();
+
+            var nameProperty = matchProperty.Property("name", StringComparison.InvariantCultureIgnoreCase);
+            if (nameProperty != null)
+            {
+                RecipeRewriter.Log.LogMessage($"Found Match Criteria 'name' of '{nameProperty.Value.Value<string>()}'");
+            }
+
+            var amountProperty = matchProperty.Property("amount", StringComparison.InvariantCultureIgnoreCase);
+            if (amountProperty != null)
+            {
+                RecipeRewriter.Log.LogMessage($"Found Match Criteria 'amount' of '{amountProperty.Value.Value<int>()}'");
+            }
+
+            var buildToolProperty = matchProperty.Property("buildTool", StringComparison.InvariantCultureIgnoreCase);
+            if (buildToolProperty != null)
+            {
+                RecipeRewriter.Log.LogMessage($"Found Match Criteria 'buildTool' of '{buildToolProperty.Value.Value<string>()}'");
+            }
+
+            return new Match(nameProperty.Value.Value<string>(), amountProperty?.Value?.Value<int>(), buildToolProperty?.Value.Value<string>() ?? null);
+        }
+    }
+}
--- JotunnModStub/RecipeRewriter.cs
+++ JotunnModStub/RecipeRewriter.cs
@@ -15,6 +15,7 @@ using Newtonsoft.Json.Linq;
 using System.Linq;
 using System.IO;
 using System.Reflection;
+using BepInEx.Logging;
 
 namespace RecipeRewriter
 {
@@ -24,11 +25,14 @@ namespace RecipeRewriter
     {
         public const string PluginGUID = "firoso.reciperewriter";
         public const string PluginName = "RecipeRewriter";
-        public const string PluginVersion = "0.0.2";
+        public const string PluginVersion = "0.1.0";
+
+        public static ManualLogSource Log { get; private set; }
 
         private void Awake()
         {
             ItemManager.OnItemsRegistered += InitializeItems;
+            Log = this.Logger;
         }
 
         private void InitializeItems()
@@ -41,156 +45,234 @@ namespace RecipeRewriter
                 }
                 catch (Exception overallException)
                 {
-                    Logger.LogError(overallException);
+                    Log.LogError(overallException);
                 }
             }
         }
 
         private void ProcessRecipeRewriteFile(string jsonDocumentFilePath)
         {
-            Logger.LogMessage($"Processing candidate RecipeRewriter file at {jsonDocumentFilePath}");
+            Log.LogMessage($"Processing candidate RecipeRewriter file at {jsonDocumentFilePath}");
 
             using (StreamReader reader = File.OpenText(jsonDocumentFilePath))
             {
                 JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
                 JArray recipes = o.Properties().SingleOrDefault(p => p.Name.Equals("recipes", StringComparison.InvariantCultureIgnoreCase)).Value as JArray;
 
-                foreach (JObject recipe in recipes.Cast<JObject>())
+                if (recipes != null)
                 {
-                    try
-                    {
-                        // match a recipe
-                        var matchProperty = recipe.Property("match", StringComparison.InvariantCultureIgnoreCase).Value.Value<JObject>();
+                    ProcessRewrites<Recipe>(recipes, GetMatchingRecipeFromObjectDb, UpdateRecipeFromJsonObject);
+                }
 
-                        var nameProperty = matchProperty.Property("name", StringComparison.InvariantCultureIgnoreCase);
-                        if (nameProperty != null)
-                        {
-                            Logger.LogMessage($"Found Recipe Rewrite for '{nameProperty.Value}'");
-                        }
+                JArray pieces = o.Properties().SingleOrDefault(p => p.Name.Equals("pieces", StringComparison.InvariantCultureIgnoreCase)).Value as JArray;
 
-                        int amountRequirement = 0;
-                        var amountProperty = matchProperty.Property("amount", StringComparison.InvariantCultureIgnoreCase);
-                        if (amountProperty != null)
-                        {
-                            Logger.LogMessage($"Found Requirement 'amount' of '{amountProperty.Value.Value<int>()}'");
-                            amountRequirement = amountProperty.Value.Value<int>();
-                        }
-
-                        string recipeItemDropName = nameProperty.Value.Value<string>();
-                        
-                        Logger.LogMessage($"Looking for a recipe to match '{recipeItemDropName}'");
-
-                        Recipe recipeToModify = ObjectDB.instance.m_recipes.SingleOrDefault(r => (r.m_item?.name?.Equals(recipeItemDropName) ?? false) && (amountProperty==null?true:r.m_amount==amountRequirement));
-                        Logger.LogMessage($"Found the recipe named '{recipeToModify.name}'");
-                        foreach (JProperty property in recipe.Properties().Where(p => !p.Name.Equals("name", StringComparison.InvariantCultureIgnoreCase)))
-                        {
-                            Logger.LogMessage($"Processing '{property.Name}'");
-
-                            switch (property.Name)
-                            {
-                                case "amount":
-                                    var baseAmount = property.Value.Value<int>();
-                                    recipeToModify.m_amount = baseAmount;
-                                    Logger.LogMessage($"Setting amount to '{baseAmount}'");
-                                    break;
-                                case "enabled":
-                                    var enabled = property.Value.Value<bool>();
-                                    recipeToModify.m_enabled = enabled;
-                                    Logger.LogMessage($"Setting enabled to '{enabled}'");
-                                    break;
-                                case "minStationLevel":
-                                    var minStationLevel = property.Value.Value<int>();
-                                    recipeToModify.m_minStationLevel = minStationLevel;
-                                    Logger.LogMessage($"Setting minStationLevel to '{minStationLevel}'");
-                                    break;
-                                case "craftingStation":
-                                    string craftingStationName = property.Value.Value<string>();
-                                    Logger.LogMessage($"Looking up craftingStation {craftingStationName}");
-
-                                    CraftingStation craftingStation = GetCraftingStationByName(craftingStationName);
-
-                                    if (craftingStation != null)
-                                    {
-                                        recipeToModify.m_craftingStation = craftingStation;
-                                        Logger.LogMessage($"Setting craftingStation to {craftingStation.name}");
-                                    }
-                                    else
-                                    {
-                                        Logger.LogError($"Unable to resolve craftingStation to {craftingStationName}");
-                                    }
-
-                                    break;
-                                case "repairStation":
-                                    var repairStationName = property.Value<string>();
-                                    Logger.LogMessage($"Looking up repairStation {repairStationName}");
-
-                                    CraftingStation repairStation = GetCraftingStationByName(repairStationName);
-
-                                    if (repairStation != null)
-                                    {
-                                        Logger.LogMessage($"Setting repairStation to {repairStation.name}");
-                                        recipeToModify.m_repairStation = repairStation;
-                                    }
-                                    else
-                                    {
-                                        Logger.LogError($"Unable to resolve repairStation to {repairStationName}");
-                                    }
-
-                                    break;
-                                case "resources":
-                                    JArray resources = (JArray)property.Value;
-
-                                    var newResouces = resources.Cast<JObject>().Select(resource =>
-                                    {
-                                        string name = resource.Property("name")?.Value.Value<string>();
-                                        int amount = resource.Property("amount")?.Value.Value<int>() ?? 0;
-                                        int amountPerLevel = resource.Property("amountPerLevel")?.Value.Value<int>() ?? 0;
-                                        bool recover = resource.Property("recover")?.Value.Value<bool>() ?? false;
-
-                                        Logger.LogMessage($"Resource | name: {name} | amount: {amount} | amountPerLevel: {amountPerLevel} | recover: {recover}");
-                                        return new Piece.Requirement { m_amount = amount, m_amountPerLevel = amountPerLevel, m_recover = recover, m_resItem = GetItemDropByName(name) };
-                                    }).ToArray();
-
-                                    recipeToModify.m_resources = newResouces;
-
-                                    break;
-                            }
-
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.LogError(ex);
-                    }
+                if (pieces != null)
+                {
+                    ProcessRewrites<Piece>(pieces, GetMatchingPieceFromObjectDb, UpdatePieceFromJsonObject);
                 }
             }
         }
 
-        private ItemDrop GetItemDropByName(string name)
+        private void ProcessRewrites<TRewrittenComponent>(JArray rewrites, Func<Match, TRewrittenComponent> componentLocator, Action<JObject, TRewrittenComponent> componentUpdater)
+        {
+            foreach (JObject recipeJObject in rewrites.Cast<JObject>())
+            {
+                try
+                {
+                    Match match = Match.FromJsonObjectWithMatchProperty(recipeJObject);
+                    Log.LogMessage($"Looking for a rewrite to match '{match.Name}' using {componentLocator.Method.Name}");
+                    TRewrittenComponent componentToModify = componentLocator(match);
+                    Log.LogMessage($"Found a matching component.");
+                    componentUpdater(recipeJObject, componentToModify);
+                }
+                catch (Exception ex)
+                {
+                    Log.LogError(ex);
+                }
+            }
+        }
+
+        private Recipe GetMatchingRecipeFromObjectDb(Match match)
+        {
+            if (match.Name == null)
+            {
+                throw new InvalidOperationException("Recipe lookup requires the 'name' property to be set for the 'match' argument.");
+            }
+
+            return ObjectDB.instance.m_recipes
+                .SingleOrDefault(r => (r.m_item?.name?.Equals(match.Name) ?? false) && (match.Amount == null ? true : r.m_amount == match.Amount));
+        }
+
+        private Piece GetMatchingPieceFromObjectDb(Match match)
+        {
+            if (match.Name == null)
+            {
+                throw new InvalidOperationException("Piece lookup requires the 'name' property to be set for the 'match' argument.");
+            }
+
+            if (match.BuildTool == null)
+            {
+                throw new InvalidOperationException("Piece lookup requires the 'buildTool' property to be set for the 'match' argument.");
+            }
+
+            var itemDrop = GetComponentFromPrefab<ItemDrop>(match.BuildTool);
+
+            var pieceTable = itemDrop.m_itemData.m_shared.m_buildPieces;
+
+            var piecePrefab = pieceTable.m_pieces.Find(piece => piece.name.Equals(match.Name, StringComparison.InvariantCultureIgnoreCase));
+
+            return GetComponentFromPrefab<Piece>(piecePrefab);
+        }
+
+
+        private void UpdateRecipeFromJsonObject(JObject recipe, Recipe recipeToModify)
+        {
+            foreach (JProperty property in recipe.Properties().Where(p => !p.Name.Equals("match", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                Log.LogMessage($"Processing '{property.Name}'");
+
+                switch (property.Name)
+                {
+                    case "amount":
+                        var baseAmount = property.Value.Value<int>();
+                        recipeToModify.m_amount = baseAmount;
+                        Log.LogMessage($"Setting amount to '{baseAmount}'");
+                        break;
+                    case "enabled":
+                        var enabled = property.Value.Value<bool>();
+                        recipeToModify.m_enabled = enabled;
+                        Log.LogMessage($"Setting enabled to '{enabled}'");
+                        break;
+                    case "minStationLevel":
+                        var minStationLevel = property.Value.Value<int>();
+                        recipeToModify.m_minStationLevel = minStationLevel;
+                        Log.LogMessage($"Setting minStationLevel to '{minStationLevel}'");
+                        break;
+                    case "craftingStation":
+                        string craftingStationName = property.Value.Value<string>();
+                        Log.LogMessage($"Looking up craftingStation {craftingStationName}");
+
+                        CraftingStation craftingStation = GetComponentFromPrefab<CraftingStation>(craftingStationName);
+
+                        if (craftingStation != null)
+                        {
+                            recipeToModify.m_craftingStation = craftingStation;
+                            Log.LogMessage($"Setting craftingStation to {craftingStation.name}");
+                        }
+                        else
+                        {
+                            Log.LogError($"Unable to resolve craftingStation to {craftingStationName}");
+                        }
+
+                        break;
+                    case "repairStation":
+                        var repairStationName = property.Value<string>();
+                        Log.LogMessage($"Looking up repairStation {repairStationName}");
+
+                        CraftingStation repairStation = GetComponentFromPrefab<CraftingStation>(repairStationName);
+
+                        if (repairStation != null)
+                        {
+                            Log.LogMessage($"Setting repairStation to {repairStation.name}");
+                            recipeToModify.m_repairStation = repairStation;
+                        }
+                        else
+                        {
+                            Log.LogError($"Unable to resolve repairStation to {repairStationName}");
+                        }
+
+                        break;
+                    case "resources":
+                        JArray resources = (JArray)property.Value;
+                        Piece.Requirement[] requirements = GetRequirementsFromJsonResources(resources);
+
+                        recipeToModify.m_resources = requirements;
+
+                        break;
+                }
+
+            }
+        }
+
+        private Piece.Requirement[] GetRequirementsFromJsonResources(JArray resources) => 
+            resources
+                .Cast<JObject>()
+                .Select(resource =>
+                {
+                    string name = resource.Property("name")?.Value.Value<string>();
+                    int amount = resource.Property("amount")?.Value.Value<int>() ?? 0;
+                    int amountPerLevel = resource.Property("amountPerLevel")?.Value.Value<int>() ?? 0;
+                    bool recover = resource.Property("recover")?.Value.Value<bool>() ?? false;
+
+                    Log.LogMessage($"Resource | name: {name} | amount: {amount} | amountPerLevel: {amountPerLevel} | recover: {recover}");
+                    return new Piece.Requirement { m_amount = amount, m_amountPerLevel = amountPerLevel, m_recover = recover, m_resItem = GetComponentFromPrefab<ItemDrop>(name) };
+                })
+                .ToArray();
+
+        private void UpdatePieceFromJsonObject(JObject pieceJObject, Piece pieceToModify)
+        {
+            foreach (JProperty property in pieceJObject.Properties().Where(p => !p.Name.Equals("match", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                Log.LogMessage($"Processing '{property.Name}'");
+
+                switch (property.Name)
+                {
+                    case "enabled":
+                        var enabled = property.Value.Value<bool>();
+                        pieceToModify.m_enabled = enabled;
+                        Log.LogMessage($"Setting enabled to '{enabled}'");
+                        break;
+                    case "craftingStation":
+                        string craftingStationName = property.Value.Value<string>();
+                        Log.LogMessage($"Looking up craftingStation {craftingStationName}");
+
+                        CraftingStation craftingStation = GetComponentFromPrefab<CraftingStation>(craftingStationName);
+
+                        if (craftingStation != null)
+                        {
+                            pieceToModify.m_craftingStation = craftingStation;
+                            Log.LogMessage($"Setting craftingStation to {craftingStation.name}");
+                        }
+                        else
+                        {
+                            Log.LogError($"Unable to resolve craftingStation to {craftingStationName}");
+                        }
+
+                        break;
+                    case "resources":
+                        JArray resources = (JArray)property.Value;
+                        Piece.Requirement[] requirements = GetRequirementsFromJsonResources(resources);
+                        pieceToModify.m_resources = requirements;
+                        break;
+                }
+            }
+        }
+
+        private GameObject GetPrefabByName(string name)
         {
             var itemPrefab = PrefabManager.Instance.GetPrefab(name);
             if (itemPrefab == null)
             {
-                throw new InvalidOperationException($"Unable to resolve item drop by from name '{name}'");
-            }
-            var itemDrop = itemPrefab.GetComponent<ItemDrop>();
-            if (itemDrop == null)
-            {
-                throw new InvalidOperationException($"Unable to resolve ItemDrop component on item named '{name}'");
+                throw new InvalidOperationException($"Unable to resolve prefab by from name '{name}'");
             }
 
-            return itemDrop;
+            return itemPrefab;
         }
 
-        private CraftingStation GetCraftingStationByName(string craftingStationName)
+        private T GetComponentFromPrefab<T>(GameObject prefab)
         {
-            Logger.LogMessage($"Looking Up Crafting Station {craftingStationName}!");
-            var craftingStationPrefab = PrefabManager.Instance.GetPrefab(craftingStationName);
-            Logger.LogMessage($"Got prefab {craftingStationPrefab?.name}!");
-            Logger.LogMessage($"Getting CraftingStation component.");
-            var craftingStation = craftingStationPrefab?.GetComponent<CraftingStation>();
-            Logger.LogMessage($"CraftingStation is {craftingStation?.name ?? "Null"}");
-            return craftingStation;
+            var component = prefab.GetComponent<T>();
+            if (component == null)
+            {
+                throw new InvalidOperationException($"Unable to resolve '{typeof(T)}' component on item named '{prefab.name}'");
+            }
+
+            return component;
+        }
+
+        private T GetComponentFromPrefab<T>(string prefabName)
+        {
+            return GetComponentFromPrefab<T>(GetPrefabByName(prefabName));
         }
     }
 }
